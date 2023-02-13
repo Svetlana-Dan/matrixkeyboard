7b6c39fd28e35e28f3c3074b91370e8be347585c
Svetlana-Dan <76607753+Svetlana-Dan@users.noreply.github.com>
2023-02-13 23:46:14 +0800

    Update sketch_feb13a.ino

--- sketch_feb13a.ino
+++ sketch_feb13a.ino
@@ -18,7 +18,11 @@ int val4;
 String result;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(led_pin3, INPUT);
+  pinMode(led_pin4, INPUT);
+  pinMode(led_pin5, OUTPUT);
+  pinMode(led_pin6, OUTPUT);
 
 }
 
@@ -37,4 +41,25 @@ void loop() {
   if (digitalRead(led_pin4) == 1 && digitalRead(led_pin3) == 0){
     result += "2";
   }
+  digitalWrite(led_pin5, LOW);
+  pinMode(led_pin5, INPUT);  
+  pinMode(led_pin6, OUTPUT);
+  digitalWrite(led_pin6, HIGH);
+  if (digitalRead(led_pin3) == 1 && digitalRead(led_pin4) == 0)
+  {
+    result += "3";
+  }
+  if (digitalRead(led_pin4) == 1 && digitalRead(led_pin3) == 0)
+  {
+    result += "4";
+  }
+  if (digitalRead(led_pin4) == 1 && digitalRead(led_pin3) == 1)
+  { 
+    result += "34";
+  }  
+  if (result != ""){
+    Serial.println(result);
+  }
+  digitalWrite(led_pin6, LOW);
+  delay(300);
 }
